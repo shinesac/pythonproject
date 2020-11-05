8b0e13cd3b3cad1e9a128bcfcd40e261ab748fe1
Shinesa Chowdhury <shinesac@yahoo.com>
2020-11-05 17:00:12 -0500

    added datetime

--- stock_value.py
+++ stock_value.py
@@ -1,5 +1,14 @@
 import math
+from datetime import datetime
 import yfinance as yf
+
+
+
+current_date = datetime.now().date()
+current_year = current_date.year
+current_month = current_date.month
+last_year = current_year - 1
+last_two_year = last_year - 1
 
 def judge_stock(): 
     if valuation < current_stock_price:
@@ -24,16 +33,15 @@ while True:
             beta = v
     current_stock_price = float(input('Enter current stock price:  '))
     expected_return = 0.08
-
-    # risk_free_rate = float(input ('Enter the current risk free rate. Use the rate of current treasury bills:  '))
     for k,v in info:
         if k == "regularMarketPrice":
             risk_free_rate = v
-    growth_rate = float(input ('Enter the anticipated growth rate:  '))
+    growth_rate = (sum(stock.dividends[str(last_year)]) - sum(stock.dividends[str(last_two_year)]))/sum(stock.dividends[str(last_two_year)])
+    print (growth_rate)
     last_dividend = float(stock.dividends[-1])
     market_cap_rate = risk_free_rate + beta*(expected_return - risk_free_rate)
     valuation = last_dividend/(market_cap_rate - growth_rate)
     judge_stock()
-    print(stock.recommendations)
+    print(stock.recommendations[str(current_date.strftime('%Y-%m'))])
 
     
