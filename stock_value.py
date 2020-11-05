1524134cd174c9736903448be7f739891849a802
Shinesa Chowdhury <shinesac@yahoo.com>
2020-11-05 12:20:58 -0500

    added master loop with break

--- stock_value.py
+++ stock_value.py
@@ -1,23 +1,31 @@
-stock = input ('Enter your stock ticker symbol:  ')
-current_stock_price = float(input('Enter current stock price:  '))
-expected_return = float(input ('Enter the expected return on the S&P 500:  '))
-beta = float (input ('Enter the beta value:  '))
-risk_free_rate = float(input ('Enter the current risk free rate. Use the rate of current treasury bills:  '))
-growth_rate = float(input ('Enter the anticipated growth rate:  '))
-last_dividend = float((input ('Enter the dividend payout for the company:  ')))
-market_cap_rate = risk_free_rate + beta*(expected_return - risk_free_rate)
-valuation = last_dividend/(market_cap_rate - growth_rate)
+import math
+import yfinance as yf
 
 def judge_stock(): 
     if valuation < current_stock_price:
-        print (stock + ' Stock is OVERVALUED')
+        print (ticker + ' Stock is OVERVALUED')
         print (valuation)
     elif valuation > current_stock_price:
-            print (stock + ' Stock is UNDERVALUED')
+            print (ticker + ' Stock is UNDERVALUED')
             print (valuation)
     else:
-            print (stock + ' Stock is at fair value')
+            print (ticker + ' Stock is at fair value')
             print (valuation)
 
-judge_stock()
+while True:    
+    ticker = input ('Enter your stock ticker symbol or type exit to exit the program:  ' )
+    if ticker == "exit":
+        break
+    stock = yf.Ticker(ticker)
+    print(stock.history('3y'))
+    current_stock_price = float(input('Enter current stock price:  '))
+    expected_return = float(input ('Enter the expected return on the S&P 500:  '))
+    beta = float (input ('Enter the beta value:  '))
+    risk_free_rate = float(input ('Enter the current risk free rate. Use the rate of current treasury bills:  '))
+    growth_rate = float(input ('Enter the anticipated growth rate:  '))
+    last_dividend = float((input ('Enter the dividend payout for the company:  ')))
+    market_cap_rate = risk_free_rate + beta*(expected_return - risk_free_rate)
+    valuation = last_dividend/(market_cap_rate - growth_rate)
+    judge_stock()
 
+    
